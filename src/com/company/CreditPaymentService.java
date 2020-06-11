cf04624b3f5d40d67ff67f66d50ddc5dc899eeff
Ruslan Badertdinov <badertdinovrr@yandex.ru>
2020-06-11 20:55:22 +0500

    Resolved problems with calculation

--- src/com/company/CreditPaymentService.java
+++ src/com/company/CreditPaymentService.java
@@ -1,25 +1,12 @@
 package com.company;
-import java.math.BigDecimal;
 
 public class CreditPaymentService {
 
-    public double creditPaymentRatio(double periodOfUse, double loanRate) {
-        double C = loanRate / 12;
-        double annuityRatio = C * (Math.pow((1 + C), periodOfUse)) / (Math.pow((1 + C), periodOfUse) - 1);
-//        int resultOfCreditPaymentRatio = (int) Math.round(annuityRatio);
-        return annuityRatio; //resultOfCreditPaymentRatio;
+    public double creditPaymentRatio(double periodOfUse, double loanRate, double loanAmount) {
+        double c = (loanRate / 100) / 12; //loan interest rate per month
+        double annuityRatio = c * (Math.pow((1 + c), periodOfUse)) / (Math.pow((1 + c), periodOfUse) - 1);
+        double mounthPayoutInDouble = annuityRatio * loanAmount;
+        int mounthPayoutInInt = (int) mounthPayoutInDouble;
+        return mounthPayoutInInt;
     }
-
-    public double creditPaymentMounth(double creditPaymentRatio, double loanAmount) { ;
-        double mounthPayout = creditPaymentRatio * loanAmount;
-//        int resultOfCreditPaymentMounth = (int) Math.round(mounthPayout);
-        return mounthPayout;
-    }
-
-    public double creditPaymentTotal(double creditPaymentMounth, double periodOfUse) {
-        double totalPayout = creditPaymentMounth * periodOfUse;
-//        int resultOfCreditPaymentTotal = (int) Math.round(totalPayout);
-        return totalPayout;
-    }
-
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,23 @@
 package com.company;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-    Scanner num = new Scanner(System.in);
-    System.out.print("Input your period of use of borrowed funds in months ");
-    double periodOfUse = num.nextDouble();
-    System.out.print("Input your loan rate ");
-    double loanRate = num.nextDouble();
-    System.out.print("Input your loan amount ");
-    double loanAmount = num.nextDouble();
+        Scanner num = new Scanner(System.in);
+        System.out.print("Input your period of use of borrowed funds in months ");
+        double periodOfUse = num.nextDouble();
+        System.out.print("Input your loan rate ");
+        double loanRate = num.nextDouble();
+        System.out.print("Input your loan amount ");
+        double loanAmount = num.nextDouble();
 
-	CreditPaymentService service = new CreditPaymentService();
+        CreditPaymentService service = new CreditPaymentService();
 
-	double ratioPayment = service.creditPaymentRatio(periodOfUse, loanRate);
-	double paymentMounth = service.creditPaymentMounth(ratioPayment, loanAmount);
-	double totalPayment = service.creditPaymentTotal(paymentMounth, periodOfUse);
+        double paymentMounth = service.creditPaymentRatio(periodOfUse, loanRate, loanAmount);
 
-	System.out.println("Your payment mounth " + paymentMounth);
-	System.out.println("Your total payment " + totalPayment);
-	System.out.println("Your credit ratio " + ratioPayment);
+        System.out.println("Your payment mounth " + paymentMounth);
     }
 }
